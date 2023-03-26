3352b2464db2dbc6f4685e416487250d315278b1
boyon99 <bo_yon_99@naver.com>
2023-03-26 15:15:09 +0900

    :hammer: feat: Change redux data

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,20 +1,45 @@
-import React, { useState } from 'react';
+import React, { } from 'react';
 import '../styles/Dropdown.scss'
+import { setSelectLocation1, setSelectLocation2 } from '../store/selectLocation';
+import { useDispatch, useSelector } from 'react-redux';
 
-const Dropdown = () => {
-  let [title, setTitle] = useState("강남구")
-  return (
-    <div>
-      <ul className="menu cleafix">
-        <li className="parent">
-          <div>{title}</div>
-          <ul className="children">
-            <li><button onClick={() => { setTitle(() => "변환") }}>강남</button></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  );
+const Dropdown = ({ list, num }) => {
+  let title = useSelector((state) => { return state.selectLocation })
+  let dispatch = useDispatch();
+  if (list) {
+    return (
+      <div>
+        <ul className="menu cleafix">
+          <li className="parent">
+            <div>{title[num]}</div>
+            <ul className="children">
+              {list.map((arr, i) => {
+                return (
+                  <li key={i}><button onClick={() => {
+                    if (num === 0) {
+                      dispatch(setSelectLocation1(arr))
+                    } else {
+                      dispatch(setSelectLocation2(arr))
+                    }
+                  }}>{arr}</button></li>
+                )
+              })}
+            </ul>
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <ul className="menu cleafix">
+          <li className="parent">
+            <div>없음</div>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 };
 
 export default Dropdown;
