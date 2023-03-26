355441610333cbeb2c49b16b33849808bf793ef2
boyon99 <bo_yon_99@naver.com>
2023-03-27 00:01:44 +0900

    :hammer: fix: Update Number => parseInt

--- src/components/Card.js
+++ src/components/Card.js
@@ -13,8 +13,11 @@ const Card = ({ data }) => {
   })
   let [star, setStar] = useState(0)
 
-  let pm25 = data.pm25Grade === null ? pm25grade[0] : (data.pm25Grade === undefined ? pm25grade[0] : pm25grade[Number(data.pm25Grade)])
-  let pm10 = data.pm10Grade === null ? pm25grade[0] : (data.pm10Grade === undefined ? pm25grade[0] : pm25grade[Number(data.pm10Grade)])
+  let pm25GradeNum = parseInt(data.pm25Grade)
+  let pm10GradeNum = parseInt(data.pm10Grade)
+
+  let pm25 = data.pm25Grade === null ? pm25grade[0] : pm25grade[pm25GradeNum]
+  let pm10 = data.pm10Grade === null ? pm25grade[0] : pm25grade[pm10GradeNum]
 
   useEffect(() => {
     setStar(likeFilter.length)
