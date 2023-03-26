96f30ea6cbf54a990f14b87ece24e6a8d9bdecf1
boyon99 <bo_yon_99@naver.com>
2023-03-26 19:30:08 +0900

    :hammer: feat: Move data.js code to App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import './styles/App.scss';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Reset } from 'styled-reset';
 import Tab from './components/Tab';
 import MyLocation from './components/MyLocation'
 import { Routes, Route } from 'react-router-dom'
 import Favorites from './components/Favorites'
 import FullMap from './components/FullMap'
-import Dropdown from './components/Dropdown';
+import axios from 'axios';
+
+const getParameters = {
+  serviceKey: 'Kr/oZVHBBoGbx2tj2VIEb91xWXHevtNNotfWYj0ZZSiZlaNDLZ6H607o6DUepfIZH7Y+ORcVESi2AsIxH8KUVA==',
+  returnType: 'json',
+  numOfRows: '10',
+  pageNo: '1',
+  sidoName: '서울',
+  ver: '1.0',
+}
 
 function App() {
   let [inner, setInner] = useState("none")
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty', { params: getParameters }
+        )
+        setData(res.data)
+        console.log(data)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    fetchData()
+  }, [])
+
   return (
     <React.Fragment>
       <Reset />
       <div className="App">
         <div className='container'>
-          <Dropdown />
           <div className='content'>
             <Routes>
-              <Route path="/" element={<MyLocation />} />
-              <Route path="/favorites" element={<Favorites />} />
-              <Route path="/fullmap" element={<FullMap />} />
+              <Route path="/" element={<MyLocation Data={data} />} />
+              <Route path="/favorites" element={<Favorites Data={data} />} />
+              <Route path="/fullmap" element={<FullMap Data={data} inner={inner} />} />
             </Routes>
           </div>
           <div className='tab-container' onMouseEnter={() => { setInner(() => "true") }} onMouseLeave={() => { setInner(() => "false") }}>
