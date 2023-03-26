57dd24c2f2cddf13d2c91a1337e8a92a52538e0c
boyon99 <bo_yon_99@naver.com>
2023-03-26 19:30:41 +0900

    :hammer: feat: Add favorites func

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,36 @@
-import React, { } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/Card.scss'
 import Pm25Grade from '../data/Pm25Grade'
+import { useSelector, useDispatch } from 'react-redux';
+import { setAdd, setDelete } from '../store/selectFavorites';
+
 
 const Card = ({ data }) => {
   let pm25grade = Pm25Grade
+  let like = useSelector((state) => { return state.selectFavorites })
+  let likeFilter = like.filter((arr) => {
+    return arr === data.stationName
+  })
+  let [star, setStar] = useState(0)
+
+  useEffect(() => {
+    setStar(likeFilter.length)
+  }
+    , [likeFilter])
+  let dispatch = useDispatch();
 
   return (
     <div className="card">
-      <div className="img-container">
-        <img src="./before.png" />
-      </div>
+      <button className="img-container" onClick={() => {
+        setStar(star === 0 ? 1 : 0)
+        if (star === 0) {
+          dispatch(setAdd(data.stationName))
+        } else {
+          dispatch(setDelete(data.stationName))
+        }
+      }}>
+        {star === 1 ? <img src="./after.png" /> : <img src="./before.png" />}
+      </button>
       <div className="card-text">
         <div className={"pm10 " + pm25grade[+data.pm25Grade]}>
           <p className='pm10-grade'><span>{pm25grade[+data.pm25Grade]}</span></p>
