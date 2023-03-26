27db27ca4f99711c20119fc0cf160cc5ab247cf6
boyon99 <bo_yon_99@naver.com>
2023-03-26 15:15:48 +0900

    :hammer: Change props data

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,44 +1,28 @@
-import React, { useState } from 'react';
+import React, { } from 'react';
 import '../styles/Card.scss'
-import Data from '../data/data.json'
-import SidoName from '../data/SidoName'
 import Pm25Grade from '../data/Pm25Grade'
 
-const Card = () => {
-  let [data] = useState(Data.response.body.items)
-  let sidoname = SidoName[1]
+const Card = ({ data }) => {
   let pm25grade = Pm25Grade
-  let filterData = data.filter((arr) => {
-    if (arr.sidoName === sidoname) {
-      return true
-    }
-  })
-  return (
-    <>
-      {
-        filterData.map((arr, i) => {
-          return (
-            <div className="card" key={i}>
-              <div className="img-container">
-                <img src="./after.png" />
-              </div>
-              <div className="card-text">
-                <div className={"pm10 " + pm25grade[+arr.pm25Grade]}>
-                  <p className='pm10-grade'><span>{pm25grade[+arr.pm25Grade]}</span></p>
-                  <p className='pm10-value'>{arr.pm10Value}</p>
-                </div>
-                <div className="title-total">
-                  <div className="title">{arr.stationName + " "}<span>{arr.sidoName}</span></div>
-                  <div className='all'>미세먼지 <span className={pm25grade[+arr.pm10Grade]}> {pm25grade[+arr.pm10Grade]}</span> <br />초미세먼지 <span className={pm25grade[+arr.pm25Grade]}>{pm25grade[+arr.pm25Grade]}</span></div>
-                  <div className="dataTime">&#40; {arr.dataTime} 기준 &#41;</div>
-                </div>
 
-              </div>
-            </div>
-          )
-        })
-      }
-    </>
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img src="./before.png" />
+      </div>
+      <div className="card-text">
+        <div className={"pm10 " + pm25grade[+data.pm25Grade]}>
+          <p className='pm10-grade'><span>{pm25grade[+data.pm25Grade]}</span></p>
+          <p className='pm10-value'>{data.pm10Value}</p>
+        </div>
+        <div className="title-total">
+          <div className="title">{data.stationName + " "}<span>{data.sidoName}</span></div>
+          <div className='all'>미세먼지 <span className={pm25grade[+data.pm10Grade]}> {pm25grade[+data.pm10Grade]}</span> <br />초미세먼지 <span className={pm25grade[+data.pm25Grade]}>{pm25grade[+data.pm25Grade]}</span></div>
+          <div className="dataTime">&#40; {data.dataTime} 기준 &#41;</div>
+        </div>
+
+      </div>
+    </div>
   );
 };
 
